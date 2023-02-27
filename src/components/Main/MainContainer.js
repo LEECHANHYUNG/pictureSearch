71ca2885d6f13dbfafc3e4ea7cd341ccc71df78f
이찬형 <lsj721538@gmail.com>
2023-02-27 14:19:08 +0000

    feat:

--- src/components/Main/MainContainer.js
+++ src/components/Main/MainContainer.js
@@ -1,20 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import PictureContainer from "./PictureContainer";
 
 const MainContainer = () => {
+  const [initialState, setInitialState] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://images-api.nasa.gov/search?q=Galaxy&page=1"
+        );
+        setInitialState(data.collection.items);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
   return (
-    <Container className="picture-container" fluid="xxl" >
-      <Col>
-        <Row>
-          <PictureContainer />
-        </Row>
-        <Row>
-          <PictureContainer />
-        </Row>
-        <Row>
-          <PictureContainer />
-        </Row>
+    <Container className="picture-container" fluid="xxl">
+      <Col style={{ paddingLeft: 0, paddingRight: 0 }}>
+        {initialState ? (
+          <Row style={{ width: "1372px", marginLeft: 0, marginRight: 0 }}>
+            <PictureContainer items={Array.from(initialState)} />
+          </Row>
+        ) : (
+          ""
+        )}
       </Col>
     </Container>
   );
--- src/components/Main/PictureContainer.js
+++ src/components/Main/PictureContainer.js
@@ -2,27 +2,26 @@ import React from "react";
 import { CardGroup, Col, Row } from "react-bootstrap";
 import PictureCard from "./PictureCard";
 
-const PictureContainer = () => {
+const PictureContainer = ({ items }) => {
   return (
     <CardGroup className="d-flex justify-content-between card-group">
-      <Col>
-        <PictureCard />
-      </Col>
-      <Col>
-        <PictureCard />
-      </Col>
-      <Col>
-        <PictureCard />
-      </Col>
-      <Col>
-        <PictureCard />
-      </Col>
-      <Col>
-        <PictureCard />
-      </Col>
-      <Col>
-        <PictureCard />
-      </Col>
+      {items.map((elem) =>
+        elem.data[0].media_type === "image" ? (
+          <Col
+            style={{
+              paddingLeft: 0,
+              paddingRight: 0,
+              width: "212px",
+              marginBottom: "20px",
+            }}
+            className="col-2"
+          >
+            <PictureCard item={elem} key={elem.data[0].nasa_id} />
+          </Col>
+        ) : (
+          ""
+        )
+      )}
     </CardGroup>
   );
 };
